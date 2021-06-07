f50dacf1974086eb4861f285d84a99d3d5ee54e0
Robert Jeffrey <IAmRobertJeffrey@gmail.com>
2021-06-07 21:48:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const cors = require("cors");
 
 //USE CORS ONCE HOSTED
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // app.use(cors);
 
