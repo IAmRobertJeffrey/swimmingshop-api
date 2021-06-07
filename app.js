d29859fcd1dca13035ee7c4a03d40ba9eb06b53a
Robert Jeffrey <IAmRobertJeffrey@gmail.com>
2021-06-07 18:20:11 +0200

    added index again

--- app.js
+++ app.js
@@ -36,4 +36,8 @@ mongoose.connect('mongodb+srv://frontend:frontend@robertsfreecluster.kkllv.mongo
 
 
 //Listen to server
-app.listen(process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
